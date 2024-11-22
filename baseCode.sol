5c75382e83a5cc8f055822a6eb26518c3b5665ac
Just_Anniee <145839789+Annieeeee11@users.noreply.github.com>
2024-11-22 17:55:10 +0530

    Add files via upload

--- baseCode.sol
+++ baseCode.sol
@@ -1,14 +1,15 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract VisibilityExample {
-
-    function internalFunction() internal pure returns(string memory) {
-        return "Internal Function Called";
+contract Parent {
+    function greet() public pure virtual returns(string memory) {
+        return "Hello from Parent";
     }
 
-    function externalFunction() external pure returns(string memory) {
-        return internalFunction();
-    }
+}
 
+contract Child is Parent{
+    function greet() public override  pure returns(string memory) {
+        return "Hello from Child";
+    }
 }
